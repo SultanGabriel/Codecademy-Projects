3ab97c0d2a35ae2874a7b0f9a17ae2058daca286
Sultan <sultan7575@protonmail.com>
2022-01-14 20:40:27 +0100

    Completed Project 2 Beat Mix

--- Project 2 Beat Mix/presetHandler.js
+++ Project 2 Beat Mix/presetHandler.js
@@ -1,8 +1,25 @@
 // Use this presets array inside your presetHandler
-const presets = require('./presets');
+const presets = require("./presets");
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (reqType, presetIndex, newPresetArray = []) => {
+  if (reqType == "GET") {
+    if (presets[presetIndex]) {
+      return [200, presets[presetIndex]];
+    } else {
+      return [404];
+    }
+  } else if (reqType == "PUT") {
+    if (presets[presetIndex]) {
+      presets[presetIndex] = newPresetArray;
+      return [200, presets[presetIndex]];
+    } else {
+      return [404];
+    }
+  } else {
+    return [400];
+  }
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
